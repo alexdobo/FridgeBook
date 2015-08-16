b0dddc79b67c2aaed600f8877a65a783df87eee0
AryaSGao <AryaSGao@users.noreply.github.com>
2015-08-16 13:24:07 +1000

    Update dailyConsumption.ino

--- dailyConsumption.ino
+++ dailyConsumption.ino
@@ -2,33 +2,28 @@ int calibrate = 1; // 0 = Output to Processing Application while 1 = Calibration
 int loadPin = 0;  // Arduino analog pin to read
 
 // LOAD CELL CALIBRATION
-#define LOAD_LOW 0
-#define ANALOG_LOW (NUM)
-#define LOAD_HIGH (NUM)
-#define ANALOG_HIGH (NUM)
+#define LOAD_LOW 0  //obtained low end load in grammes from good scales
+#define ANALOG_LOW (NUM) //analog reading obtained from experiments
+#define LOAD_HIGH (NUM) //obtained high end load in grammes from good scales
+#define ANALOG_HIGH (NUM) //analog reading from load cell for high end test load
 
-static long loadLow = LOAD_LOW; // measured low end load in grammes from good scales
-static int analogLow = ANALOG_LOW   ; // analog reading obtained from experiments
-static long loadHigh = LOAD_HIGH ; //  obtained high end load in grammes from good scales
-static int analogHigh = ANALOG_HIGH ; //  analog reading from load cell for high end test load
-
-// This is used when you change the load cell platform to something else that weighs
-// different and the load is no longer on zero. Add an offset to set to zero.
-int loadAdjustment = LOAD_LOW;  //  Adjust non loaded load cell to 0
+static long loadLow = LOAD_LOW; 
+static int analogLow = ANALOG_LOW; 
+static long loadHigh = LOAD_HIGH ; 
+static int analogHigh = ANALOG_HIGH ; 
 
 
-
+//This is to avoid measurement bias and adjust no load to zero
+int loadAdjustment = LOAD_LOW;  
+int initLoad = 2000; //assume the user always buys 2kg milk
 
 // Values for the analog sample buffer for running average 
-static int samplesBuffer = 100;  // Should be the same as the number of samples
-int analogSamples[100] = {0};  // Number of analog samples to average and set to 0
+static int samplesBuffer = 14;  // 2 wks
+int analogSamples[14] = {0};  // all init-ed as 0
 int lastSampleIndex = 0; // Last analog sample
 
 long sampleBufferTotal = 0; // The sum of all the samples held in the buffer
 long analogSamplesAverage = 0; // Running average of the sum of the buffer
-
-// Results plot or display frequency
-long time = 0;  // Set time to mark start of delay
 
 
 void setup() {
@@ -43,22 +38,17 @@ void setup() {
 void loop() {
 
   //an old value here
-  
+  int initValue
   //a new value
-  
-  //subtract
-  
-  //add sample
-  
-  //the new value becomes the new old value 
-  
-  //get average
-  
   int loadValue = analogRead(loadPin);  // Get analog read
-  int oldestSample = addSample(loadValue);
-  
-  // Get running average, pass the latest and oldest analog sample reading
- samplesAverage = runningAverage(loadValue, oldestSample);
+  //subtract
+  int sampleValue = initValue - loadValue;
+  //add sample
+   int oldestSample = addSample(sampleValue);
+  //the new value becomes the new old value 
+  initValue = loadValue;
+  //get average
+  samplesAverage = runningAverage(loadValue, oldestSample);
   
   /*if debug
     if (calibrate) {// print test results
